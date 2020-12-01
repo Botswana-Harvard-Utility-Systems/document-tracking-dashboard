2bb9a3a2837b667a58748c76308b093f57152f42
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2020-12-01 08:54:43 +0200

    Filter documents by user group

---
+++ document_tracking_dashboard/views/document/group_documents_listboard_view.py
@@ -0,0 +1,71 @@
+import re
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
+from django.db.models import Q
+from django.utils.decorators import method_decorator
+from edc_base.view_mixins import EdcBaseViewMixin
+from edc_dashboard.view_mixins import ListboardFilterViewMixin, SearchFormViewMixin
+from edc_dashboard.views import ListboardView
+from edc_navbar import NavbarViewMixin
+
+from document_tracking.models import SendDocument
+from ...model_wrappers import SentDocumentModelWrapper
+
+
+class GroupDocumentsListBoardView(
+        NavbarViewMixin, EdcBaseViewMixin, ListboardFilterViewMixin,
+        SearchFormViewMixin, ListboardView):
+
+    listboard_template = 'sent_document_listboard_template'
+    listboard_url = 'group_documents_listboard_url'
+    listboard_panel_style = 'info'
+    listboard_fa_icon = "fas fa-file"
+
+    model = 'document_tracking.senddocument'
+    model_wrapper_cls = SentDocumentModelWrapper
+    navbar_name = 'document_tracking_dashboard'
+    navbar_selected_item = 'Document'
+    ordering = '-modified'
+    paginate_by = 10
+    search_form_url = 'group_documents_listboard_url'
+
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def document(self, doc_identifier=None):
+        """Reeturn a new contract obj.
+        """
+        return SentDocumentModelWrapper(SendDocument(doc_identifier=doc_identifier))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        doc_identifier = kwargs.get('doc_identifier', None)
+        context.update(
+            doc_identifier=doc_identifier,
+            document=self.document(doc_identifier=doc_identifier),
+        )
+        return context
+
+    def get_queryset_filter_options(self, request, *args, **kwargs):
+        options = super().get_queryset_filter_options(request, *args, **kwargs)
+        if kwargs.get('doc_identifier'):
+            options.update(
+                {'doc_identifier': kwargs.get('doc_identifier')})
+
+        groups = request.user.groups.all()
+        groups_as_list = list(groups)
+
+        if groups_as_list:
+            for group_item in groups_as_list:
+
+                    options.update(
+                        {'group': group_item.id})
+
+        return options
+
+    def extra_search_options(self, search_term):
+        q = Q()
+        if re.match('^[A-Z]+$', search_term):
+            q = Q(doc_identifier__exact=search_term)
+        return q
