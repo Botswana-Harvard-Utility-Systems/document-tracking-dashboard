7fc6c578214f99de763f0939c4b027871f271dfa
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2020-12-14 23:17:39 +0200

    fixing navbars

--- document_tracking_dashboard/navbars.py
+++ document_tracking_dashboard/navbars.py
@@ -8,16 +8,6 @@ document_tracking_dashboard = Navbar(name='document_tracking_dashboard')
 
 document_tracking_dashboard.append_item(
     NavbarItem(
-        name='procurement',
-        title='Procurement',
-        label='Procurement',
-        fa_icon='fa fa-list-alt',
-        url_name=settings.DASHBOARD_URL_NAMES[
-            'procurement_url'],
-        no_url_namespace=no_url_namespace))
-
-document_tracking_dashboard.append_item(
-    NavbarItem(
         name='documents',
         title='Documents',
         label='Documents',
@@ -28,13 +18,22 @@ document_tracking_dashboard.append_item(
 
 document_tracking_dashboard.append_item(
     NavbarItem(
-        name='CMS',
-        title='CMS',
-        label='CMS',
-        fa_icon='',
+        name='sent_documents',
+        title='Sent Documents',
+        label='Sent Documents',
+        fa_icon='far fa-file',
         url_name=settings.DASHBOARD_URL_NAMES[
-            'cms_url'],
+            'sent_listboard_url'],
         no_url_namespace=no_url_namespace))
 
+document_tracking_dashboard.append_item(
+    NavbarItem(
+        name='documents_sent_to_me',
+        title='Documents Sent To Me',
+        label='Documents Sent To Me',
+        fa_icon='fas fa-inbox',
+        url_name=settings.DASHBOARD_URL_NAMES[
+            'sent_to_me_listboard_url'],
+        no_url_namespace=no_url_namespace))
 
 site_navbars.register(document_tracking_dashboard)
