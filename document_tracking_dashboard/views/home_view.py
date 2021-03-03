c6871d663cc8336d18a508d8e511aee0382df2c2
ThatoKgamaetsile <thatokgamaetsile@gmail.com>
2021-03-03 12:50:58 +0200

    Update Receptions

--- document_tracking_dashboard/views/home_view.py
+++ document_tracking_dashboard/views/home_view.py
@@ -15,7 +15,8 @@ class HomeView(EdcBaseViewMixin, NavbarViewMixin, TemplateView):
 
     @property
     def is_receptionist(self):
-        reception_groups = ['BHP HQ', 'Finance Reception', 'CTU Reception']
+        reception_groups = ['BHP HQ Reception', 'Finance Reception',
+                            'CTU Reception']
         return self.request.user.groups.filter(
             name__in=reception_groups).count() != 0
 
